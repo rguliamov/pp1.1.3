c7be19d81a271930720fbf11accbfe0a80d3c3e7
Rustam <gulyamov1917@gmail.com>
2022-09-29 01:38:19 +0300

    GulyamovRustam29.09

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,103 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Util util = new Util();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
+        String sql = "CREATE TABLE USER (" +
+                "id bigint  PRIMARY KEY AUTO_INCREMENT," +
+                "name varchar(32) NOT NULL," +
+                "lastName varchar(32) NOT NULL," +
+                "age integer," +
+                "check (age >= 0 AND age <= 127))";
 
+        try {
+            util.statement(stmt -> {
+                stmt.executeUpdate(sql);
+            });
+        } catch (SQLException e) {
+        }
     }
 
-    public void dropUsersTable() {
 
+    public void dropUsersTable() {
+        String sql = "DROP TABLE USER";
+        try {
+            util.statement(stmt -> {
+                stmt.executeUpdate(sql);
+            });
+        } catch (SQLException e) {
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO USER(name, lastName, age) values (?, ?, ?)";
+        try {
+            util.preparedStatement(sql, stmt -> {
+                stmt.setString(1, name);
+                stmt.setString(2, lastName);
+                stmt.setByte(3,age);
+                stmt.executeUpdate();
+            });
+        } catch (SQLException e) {
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sql = "DELETE FROM USER WHERE id=?";
+        try {
+            util.preparedStatement(sql, stmt -> {
+                stmt.setLong(1, id);
+                stmt.executeUpdate();
+            });
+        } catch (SQLException e) {
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sql = "SELECT * FROM USER";
+        List<User> users = new ArrayList<>();
+        try {
+            util.statement(stmt -> {
+                ResultSet resultSet = stmt.executeQuery(sql);
+
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getLong(1));
+                    user.setName(resultSet.getString(2));
+                    user.setLastName(resultSet.getString(3));
+                    user.setAge(resultSet.getByte(4));
+                    users.add(user);
+                }
+            });
+        } catch (SQLException e) {
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        String sql = "DELETE FROM USER";
+        try {
+            util.statement(stmt -> {
+                stmt.executeUpdate(sql);
+            });
+        } catch (SQLException e) {
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,69 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.xdevapi.SqlStatement;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    @FunctionalInterface
+    public interface SQLFunction<T, R> {
+        R apply(T object) throws SQLException;
+    }
+
+    @FunctionalInterface
+    public interface  SQLConsumer<T> {
+        void accept(T object) throws SQLException;
+    }
+
+    private static HikariConfig config = new HikariConfig("/hikari.properties");
+    private static DataSource connectionPool = new HikariDataSource(config);
+
+    public void connection(SQLConsumer<? super Connection> consumer) throws SQLException {
+        Objects.requireNonNull(consumer);
+        try (Connection connection = connectionPool.getConnection()) {
+            consumer.accept(connection);
+        }
+    }
+
+    public <R> R connection(SQLFunction<? super Connection, ? extends R> function) throws SQLException {
+        Objects.requireNonNull(function);
+        try (Connection connection = connectionPool.getConnection()) {
+            return function.apply(connection);
+        }
+    }
+
+    public <R> R statement(SQLFunction<? super Statement, ? extends R> function) throws SQLException {
+        Objects.requireNonNull(function);
+        return connection(conn -> {
+            try (Statement statement = conn.createStatement()) {
+                return function.apply(statement);
+            }
+        });
+    }
+
+    public void statement(SQLConsumer<? super Statement> consumer) throws SQLException {
+        Objects.requireNonNull(consumer);
+        connection(conn -> {
+            try (Statement statement = conn.createStatement()) {
+                consumer.accept(statement);
+            }
+        });
+    }
+
+    public void preparedStatement(String sql, SQLConsumer<? super PreparedStatement> consumer) throws SQLException {
+        Objects.requireNonNull(consumer);
+        connection(conn -> {
+            try (PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
+                consumer.accept(preparedStatement);
+            }
+        });
+    }
 }
